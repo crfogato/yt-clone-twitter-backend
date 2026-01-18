9168f7b5ef1a1f229f4474947d326616684926e8
crfogato <crfogato@gmail.com>
2026-01-18 18:08:47 -0300

    Add basic server structure: server.ts, controllers, routers, utils

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import { mainRouter } from "./routers/main.js";
+
+const server = express();
+server.use(helmet());
+server.use(cors());
+server.use(express.urlencoded({ extended: true }));
+server.use(express.json());
+
+// rotas
+server.use(mainRouter);
+
+server.listen(process.env.PORT || 3000, () => {
+  console.log(`Servidor rodando em ${process.env.BASE_URL}`);
+});
